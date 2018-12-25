184786877042d03f5a6a008919abf3d473704e72
Illia Shabaiev <ilyashabaev2209@gmail.com>
2018-12-25 16:38:11 -0600

    added class Point, method distance

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,14 @@
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance (Point p1, Point p2){
+        return  Math.sqrt((p2.x - p1.x) + (p2.y - p1.y));
+    }
+
+}
--- sandbox/src/main/java/Program.java
+++ sandbox/src/main/java/Program.java
@@ -1,5 +1,8 @@
 class Program {
     public static void main(String[] args) {
+        Point one = new Point(5,22.34);
+        Point two = new Point(14.5,17);
+        System.out.println(String.format("%.2f",Point.distance(one, two)));
         System.out.println("Hello world!");
     }
 }
