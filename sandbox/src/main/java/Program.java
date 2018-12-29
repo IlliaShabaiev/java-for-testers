073bac8d27bfb915036ba426d9251a143c285606
Illia Shabaiev <ilyashabaev2209@gmail.com>
2018-12-29 12:14:32 -0600

    Исправлена ошибка в формуле

--- sandbox/src/main/java/Program.java
+++ sandbox/src/main/java/Program.java
@@ -3,7 +3,7 @@ class Program {
       Point p1 = new Point(11, 12);
       Point p2 = new Point(12, 13);
       p1.distance(p2);
-        System.out.println(String.format("%.2f", p1.distance(p2)));
+        System.out.println(String.format("%.3f", p1.distance(p2)));
         System.out.println("Hello world!");
       }
     }
