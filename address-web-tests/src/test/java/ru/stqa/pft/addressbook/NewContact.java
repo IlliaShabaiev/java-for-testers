84fa6102c47c2fe9a03e0a5337ad7246be79e112
Illia Shabaiev <ilyashabaev2209@gmail.com>
2019-01-10 16:38:07 -0600

    Создание параметров

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/NewContact.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/NewContact.java
@@ -15,46 +15,66 @@ public class NewContact {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/edit.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
   public void testNewContact() throws Exception {
+    EnterNewContactInformation(new NewContactData("Ilya", "Shabaev", "Sidley", "1212 Granville", "123456789", "8728003751", "ilyashabaev2209@gmail.com"));
+    SubmitNewContactCreation();
+    reutrnToTheHomePage();
+    LogOut();
+  }
+
+  private void LogOut() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void reutrnToTheHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void SubmitNewContactCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void EnterNewContactInformation(NewContactData newContactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Ilya");
+    wd.findElement(By.name("firstname")).sendKeys(newContactData.getFirstName());
     wd.findElement(By.name("theform")).click();
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Shabaev");
+    wd.findElement(By.name("lastname")).sendKeys(newContactData.getLastName());
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("Sidley");
+    wd.findElement(By.name("company")).sendKeys(newContactData.getCompany());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("1212 Granville");
+    wd.findElement(By.name("address")).sendKeys(newContactData.getAddress());
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("123456789");
+    wd.findElement(By.name("home")).sendKeys(newContactData.getHomeNumber());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("8728003751");
+    wd.findElement(By.name("mobile")).sendKeys(newContactData.getMobileNumber());
     wd.findElement(By.name("theform")).click();
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("ilyashabaev2209@gmail.com");
+    wd.findElement(By.name("email")).sendKeys(newContactData.getEmail());
     wd.findElement(By.name("theform")).click();
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    wd.findElement(By.linkText("home page")).click();
-    wd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)
