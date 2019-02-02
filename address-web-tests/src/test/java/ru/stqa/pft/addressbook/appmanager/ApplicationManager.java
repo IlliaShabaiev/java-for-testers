7f10d5f9f1f6f7aff6652db051005f1cf806cc23
Illia Shabaiev <ilyashabaev2209@gmail.com>
2019-02-01 20:37:38 -0600

    Запуск в разных браузерах

--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,25 +2,45 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.remote.BrowserType;
+
+import sun.plugin2.main.client.WDonatePrivilege;
+
 
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-
-
-  private WebDriver wd;
-  private WebDriverWait wait;
+  WebDriver wd;
+  WebDriverWait wait;
 
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private DataHelper dataHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-   wd = new ChromeDriver();
+
+    if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    }else if (browser.equals (BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    }else if (browser.equals(BrowserType.IE)) {
+
+      wd = new InternetExplorerDriver();
+    }
+
+
+
    wait = new WebDriverWait(wd,30);
    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
    wd.get("http://localhost/addressbook/");
--- address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ address-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -8,10 +8,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod    ;
 import ru.stqa.pft.addressbook.model.NewContactData;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+
  // protected WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
